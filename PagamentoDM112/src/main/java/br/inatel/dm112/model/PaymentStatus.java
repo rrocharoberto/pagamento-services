1d8f9864876937e771958131b7d1c2e2d4d1843a
Roberto Rocha <rrocha.roberto@gmail.com>
2024-02-25 22:07:51 -0300

    Improve payment processing logic

--- PagamentoDM112/src/main/java/br/inatel/dm112/model/PaymentStatus.java
+++ PagamentoDM112/src/main/java/br/inatel/dm112/model/PaymentStatus.java
@@ -1,9 +1,11 @@
 package br.inatel.dm112.model;
 
+import br.inatel.dm112.rest.support.PaymentException;
+
 public class PaymentStatus {
 
 	public enum PAY_STATUS {
-		OK, NULL_VALUES, ORDER_NOT_FOUND, WRONG_ORDER_STATUR, ORDER_ERROR, BILLET_ERROR, EMAIL_ERROR
+		OK, NULL_VALUES, ORDER_NOT_FOUND, WRONG_ORDER_STATUS, ORDER_ERROR, BILLET_ERROR, EMAIL_ERROR
 	}
 
 	private String cpf;
@@ -20,8 +22,10 @@ public class PaymentStatus {
 		this.orderNumber = orderNumber;
 	}
 
-	public static PaymentStatus createErrorStatus(String cpf, int orderNumber, PAY_STATUS errorStatus) {
-		return new PaymentStatus(errorStatus.ordinal(), cpf, orderNumber);
+	public static PaymentException createErrorStatus(String msg, String cpf, int orderNumber, PAY_STATUS errorStatus) {
+		System.out.println(msg);
+		return new PaymentException(msg + " Details: error status " + errorStatus.name()
+				+ " CPF: " + cpf + " Order: " + orderNumber);
 	}
 
 	public int getStatus() {
--- PagamentoDM112/src/main/java/br/inatel/dm112/services/PaymentService.java
+++ PagamentoDM112/src/main/java/br/inatel/dm112/services/PaymentService.java
@@ -1,7 +1,5 @@
 package br.inatel.dm112.services;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,16 +19,13 @@ public class PaymentService {
 
 	@Autowired
 	private BilletClient clientBillet;
-	
+
 	@Autowired
 	private EmailClient clientEmail;
 
 	/**
-	 * Lógica de geração de pendência de pagamento
-	 * (1) consulta o pedido pelo número
-	 * (2) atualiza o status do pedido
-	 * (3) gera o boleto
-	 * (4) envia email com o pdf
+	 * Lógica de geração de pendência de pagamento (1) consulta o pedido pelo número
+	 * (2) atualiza o status do pedido (3) gera o boleto (4) envia email com o pdf
 	 * (5) retorna sucesso
 	 * 
 	 * @param cpf
@@ -38,83 +33,78 @@ public class PaymentService {
 	 * @return
 	 */
 	public PaymentStatus startPaymentOfOrder(String cpf, int orderNumber) {
-		
-		if (cpf == null || orderNumber < 0) {
-			return PaymentStatus.createErrorStatus(cpf, orderNumber, PAY_STATUS.NULL_VALUES);
-		}
-		Order order;
-		try {
-			order = clientOrder.retrieveOrder(orderNumber); //(1) consulta o pedido pelo número
-		} catch(Exception e ) {
-			System.out.println("Order " + orderNumber + " not found.");
-			return PaymentStatus.createErrorStatus(cpf, orderNumber, PAY_STATUS.ORDER_NOT_FOUND);
-		}
-		
-		if(order.getStatus() != Order.STATUS.PENDING.ordinal()) {
-			System.out.println("Invalid order status: " + orderNumber + ": " + order.getStatus());
-			return PaymentStatus.createErrorStatus(cpf, orderNumber, PAY_STATUS.WRONG_ORDER_STATUR);
+
+		Order order = getOrder(cpf, orderNumber); // (1) consulta o pedido pelo número
+
+		if (order.getStatus() != Order.STATUS.PENDING.ordinal()) {
+			String msg = "Status do pedido " + orderNumber + " inválido: " + order.getStatus();
+			throw PaymentStatus.createErrorStatus(msg, cpf, orderNumber, PAY_STATUS.WRONG_ORDER_STATUS);
 		}
 
-		order.setIssueDate(new Date());
-		order.setStatus(Order.STATUS.PENDING.ordinal()); //pendente de pagamento
-		
 		try {
-			clientOrder.updateOrder(order); //(2) atualiza o status do pedido
-		} catch(Exception e ) {
-			System.out.println("Erro no serviço de pedido: update");
-			return PaymentStatus.createErrorStatus(cpf, orderNumber, PAY_STATUS.ORDER_ERROR);
+			clientOrder.startOrderPayment(orderNumber); // (2) atualiza o status do pedido
+		} catch (Exception e) {
+			String msg = "Erro no serviço de pedido: start payment";
+			throw PaymentStatus.createErrorStatus(msg, cpf, orderNumber, PAY_STATUS.ORDER_ERROR);
 		}
 		BilletGenResponse respBillet;
 		try {
-			respBillet = clientBillet.callGenerateBilletService(orderNumber, cpf); //(3) gera o boleto
-		} catch(Exception e ) {
-			System.out.println("Erro no serviço de boleto");
-			return PaymentStatus.createErrorStatus(cpf, orderNumber, PAY_STATUS.BILLET_ERROR);
+			respBillet = clientBillet.callGenerateBilletService(orderNumber, cpf); // (3) gera o boleto
+		} catch (Exception e) {
+			String msg = "Erro no serviço de boleto";
+			throw PaymentStatus.createErrorStatus(msg, cpf, orderNumber, PAY_STATUS.BILLET_ERROR);
 		}
-		
-		byte [] PDFContent = respBillet.getPdfContent();
+
+		byte[] PDFContent = respBillet.getPdfContent();
 		try {
-			clientEmail.callSendMailService(orderNumber, PDFContent); //(4) envia email com o pdf
-		} catch(Exception e ) {
-			System.out.println("Erro no serviço de email");
-			return PaymentStatus.createErrorStatus(cpf, orderNumber, PAY_STATUS.EMAIL_ERROR);
+			clientEmail.callSendMailService(orderNumber, PDFContent); // (4) envia email com o pdf
+		} catch (Exception e) {
+			String msg = "Erro no serviço de email";
+			throw PaymentStatus.createErrorStatus(msg, cpf, orderNumber, PAY_STATUS.EMAIL_ERROR);
 		}
 		System.out.println("Sucesso ao inicializar o pagamento: orderNumber: " + orderNumber + " cpf: " + cpf);
-		return new PaymentStatus(PAY_STATUS.OK.ordinal(), cpf, orderNumber); //(5) retorna sucesso
+		return new PaymentStatus(PAY_STATUS.OK.ordinal(), cpf, orderNumber); // (5) retorna sucesso
 	}
 
 	/**
-	 * Lógica de confirmação de pagamento
-	 * (1) consulta o pedido pelo número
-	 * (2) confirma o pagamento
-	 * (3) atualiza o status do pedido
-	 * (4) responde Ok
+	 * Lógica de confirmação de pagamento (1) consulta o pedido pelo número (2)
+	 * atualiza o status do pedido confirmando o pagamento (3) responde Ok
 	 * 
 	 * @param cpf
 	 * @param orderNumber
 	 * @return
 	 */
 	public PaymentStatus confirmPaymentOfOrder(String cpf, int orderNumber) {
-		
-		if (cpf == null || orderNumber < 0) {
-			return PaymentStatus.createErrorStatus(cpf, orderNumber, PAY_STATUS.NULL_VALUES);
-		}
-		
-		Order order = clientOrder.retrieveOrder(orderNumber); //(1) consulta o pedido pelo número
 
-		if(order == null) { //alguma hora vai ser preciso verificar o status do pedido aqui
-			System.out.println("Erro no serviço de pedido: order is null.");
-			return PaymentStatus.createErrorStatus(cpf, orderNumber, PAY_STATUS.ORDER_NOT_FOUND);
+		Order order = getOrder(cpf, orderNumber); // (1) consulta o pedido pelo número
+
+		if (order.getStatus() == Order.STATUS.CONFIRMED.ordinal()) {
+			String msg = "Status do pedido " + orderNumber + " inválido: " + order.getStatus();
+			throw PaymentStatus.createErrorStatus(msg, cpf, orderNumber, PAY_STATUS.WRONG_ORDER_STATUS);
 		}
-		order.setPaymentDate(new Date());
-		order.setStatus(Order.STATUS.CONFIRMED.ordinal()); //(2) confirma o pagamento
 		try {
-			clientOrder.updateOrder(order); //(3) atualiza o status do pedido
-		} catch(Exception e ) {
-			System.out.println("Erro no serviço de pedido: update");
-			return PaymentStatus.createErrorStatus(cpf, orderNumber, PAY_STATUS.ORDER_ERROR);
+			clientOrder.confirmOrderPayment(orderNumber); // (2) confirma o pagamento (e atualiza o status)
+		} catch (Exception e) {
+			String msg = "Erro no serviço de pedido: confirm payment";
+			throw PaymentStatus.createErrorStatus(msg, cpf, orderNumber, PAY_STATUS.ORDER_ERROR);
 		}
 		System.out.println("Sucesso ao confirmar o pagamento: orderNumber: " + orderNumber + " cpf: " + cpf);
-		return new PaymentStatus(PAY_STATUS.OK.ordinal(), cpf, orderNumber); //(4) responde Ok
+		return new PaymentStatus(PAY_STATUS.OK.ordinal(), cpf, orderNumber); // (3) responde Ok
 	}
+
+	private Order getOrder(String cpf, int orderNumber) {
+		if (cpf == null || orderNumber < 0) {
+			throw PaymentStatus.createErrorStatus("CPF e pedido são obrigatórios", cpf, orderNumber,
+					PAY_STATUS.NULL_VALUES);
+		}
+		Order order;
+		try {
+			order = clientOrder.retrieveOrder(orderNumber); // (1) consulta o pedido pelo número
+		} catch (Exception e) {
+			String msg = "Pedido " + orderNumber + " não encontrado.";
+			throw PaymentStatus.createErrorStatus(msg, cpf, orderNumber, PAY_STATUS.ORDER_NOT_FOUND);
+		}
+		return order;
+	}
+
 }
